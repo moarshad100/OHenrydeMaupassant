cb52afcba3019d0fb5ffd8b7b0def72a826ff6f4
moarshad100 <mo.arshad100@gmail.com>
2023-07-09 20:28:33 +1000

    initial commit

---
+++ script.js
@@ -0,0 +1,43 @@
+var storySearch = document.getElementById('search_button');
+var storyInput = document.getElementById('searchInput');
+var storyDisplay = document.getElementById('display_text');
+var addSelect = document.getElementById('Stories');
+var addStories = document.getElementById('check_story');
+
+
+var requestUrl = "https://shortstories-api.onrender.com/stories";
+
+
+fetch(requestUrl)
+  .then(function(response){
+  return response.json();
+  })
+  .then(function(data){
+    console.log(data);
+
+    
+
+    for(i=0;i<data.length;i++){
+      var createStoriesOptions = document.createElement('option');
+      createStoriesOptions.textContent = data[i].title;
+      addSelect.appendChild(createStoriesOptions);
+    }
+
+    $(storySearch).on("click",function(){
+      $(addStories).empty();
+      var e = document.getElementById('Stories');
+      var createPara = document.createElement('p');
+      console.log(e.value);
+      for(i=0;i<data.length;i++){
+      if(e.value === data[i].title){
+        createPara.textContent = data[i].story;
+        addStories.appendChild(createPara);
+
+      }
+    }
+      
+    });
+
+
+
+  });
